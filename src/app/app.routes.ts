2de6b865e06b42c4419ea3d8ee5e6febc4567a92
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-09-28 19:05:25 +0200

    Added Components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ToursComponent } from './tours/tours.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { CartComponent } from './cart/cart.component';
+import { ProfileComponent } from './profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/tours', pathMatch: 'full'},
+    { path: 'home', component: ToursComponent},
+    { path: 'tours', component: ToursComponent},    
+    { path: 'favorites', component: FavoritesComponent},    
+    { path: 'cart', component: CartComponent},    
+    { path: 'profile', component: ProfileComponent},
+];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderButtonComponent } from '../header-button/header-button.component'
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,28 +12,31 @@ import { CommonModule } from '@angular/common';
 })
 
 export class HeaderComponent {
-
   public headerButtons = [
+    { icon: 'home', text: 'Home', action: () => this.onHomeClick() },
     { icon: 'favorite', text: 'Favorites', action: () => this.onFavoriteClick() },
     { icon: 'shopping_cart', text: 'Cart', action: () => this.onCartClick() },
     { icon: 'person', text: 'Profile', action: () => this.onProfileClick() },
     ];
 
+  constructor(private router: Router){}
+
   onFavoriteClick() {
     console.log('Favorite clicked');
-    // Add your logic here
-  }
+    this.router.navigate(['favorites']);  }
 
   onCartClick() {
     console.log('Cart clicked');
-    // Add your logic here
+    this.router.navigate(['cart']);
   }
   
   onProfileClick() {
     console.log('Profile clicked');
+    this.router.navigate(['profile']);
   }
   
   onHomeClick() {
     console.log('Home clicked');
+    this.router.navigate(['home']);
   }    
 }
