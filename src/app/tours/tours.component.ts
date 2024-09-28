d599eeb02182c71bcc7400089c544b371cc359d3
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-09-28 19:22:55 +0200

    First try with angular materials

--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCard, MatCardContent, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-tours',
   standalone: true,
-  imports: [],
+  imports: [MatSlideToggleModule,MatCard,MatCardContent, MatCardSubtitle, MatCardTitle],
   templateUrl: './tours.component.html',
   styleUrl: './tours.component.css'
 })
