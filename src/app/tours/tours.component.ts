1ac7b99175761186e6ec5b42ca026fe10611a294
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-10-02 18:42:19 +0200

    Make list of carousels and dummy data. Also added some favorite for carusel item logic

--- src/app/tours/tours.component.ts
+++ src/app/tours/tours.component.ts
@@ -1,22 +1,41 @@
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCard, MatCardContent, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbCarouselModule, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tours',
   standalone: true,
-  imports: [NgbCarouselModule, FormsModule, MatSlideToggleModule, MatCard, MatCardContent, MatCardSubtitle, MatCardTitle],
+  imports: [CommonModule, NgbCarouselModule, FormsModule, MatSlideToggleModule, MatCard, MatCardContent, MatCardSubtitle, MatCardTitle],
   templateUrl: './tours.component.html',
   styleUrl: './tours.component.css'
 })
-export class ToursComponent {
+export class ToursComponent implements OnInit {
+
   images = [
     '../assets/img/albanien-urlaub.jpg',
     '../assets/img/albanien-urlaub2.jpg',
   ]
 
+  carouselList: CarouselItem[] = [
+    {
+      images: [
+        '../assets/img/albanien-urlaub.jpg',
+        '../assets/img/albanien-urlaub2.jpg'], interval: 0
+    },
+    {
+      images: [
+        '../assets/img/albanien-urlaub.jpg',
+        '../assets/img/albanien-urlaub2.jpg'], interval: 0
+    },
+    {
+      images: [
+        '../assets/img/albanien-urlaub.jpg',
+        '../assets/img/albanien-urlaub2.jpg'], interval: 0
+    }
+  ];
 
   paused = false;
   unpauseOnArrow = false;
@@ -25,6 +44,18 @@ export class ToursComponent {
   pauseOnFocus = true;
 
   @ViewChild('carousel', { static: true }) carousel: NgbCarousel | undefined;
+
+  private readonly maxRandomInterval = 10000;
+  private readonly minRandomInterval = 5000;
+
+  favorites: Set<number> = new Set(); // Store favorite slide indices
+  showNavigationArrows: boolean = true;
+
+  ngOnInit(): void {
+    this.carouselList.forEach(carouselItem => {
+      carouselItem.interval = this.getRandomInterval();
+    });
+  }
 
   togglePaused() {
     if (this.paused) {
@@ -52,4 +83,33 @@ export class ToursComponent {
       this.togglePaused();
     }
   }
+
+
+  getRandomInterval(): number {
+    return Math.floor(Math.random() * (this.maxRandomInterval - this.minRandomInterval + 1)) + this.minRandomInterval;
+  }
+
+  // hideNavigationArrows() {
+  //   this.showNavigationArrows = false;
+  // }
+  // unhideNavigationArrows() {
+  //   this.showNavigationArrows = true;
+  // }
+
+  toggleFavorite(index: number) {
+    if (this.favorites.has(index)) {
+      this.favorites.delete(index);
+    } else {
+      this.favorites.add(index);
+    }
+  }
+
+  isFavorite(index: number): boolean {
+    return this.favorites.has(index);
+  }
+
+}
+export interface CarouselItem {
+  images: string[];
+  interval: number;
 }
