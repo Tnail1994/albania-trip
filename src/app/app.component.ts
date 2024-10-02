8ff52cc9db024de7450376fe2435ed01bac61d30
Stefano <62762187+Tnail1994@users.noreply.github.com>
2024-10-02 17:14:13 +0200

    Added bootstrap and first page implementations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { CommonModule } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent,MatIconModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'albania-trip';
+  tabs: any;
+  
 }
